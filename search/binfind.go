84dc65adad517de093949d4c2fe496c55b8c1461
Audio-sohn <info@audiosohn.com>
2025-02-16 15:34:02 +0100

    binfind updated

--- search/binfind.go
+++ search/binfind.go
@@ -1,9 +1,23 @@
 package search
 
+import "aufgaben-rekursion/lists"
+
 // FindSorted sucht in einer sortierten Liste nach dem ersten Vorkommen von x.
 // Falls x nicht gefunden wird, wird -1 zurückgegeben.
 // Da die Liste sortiert ist, wird die binäre Suche verwendet.
 func FindSorted(list []int, x int) int {
-	// TODO
+
+	// Rekursionsanker: wenn liste leer, dann -1 returnen
+	if lists.Empty(list) {
+
+		return -1
+
+	}
+
+	// Rekursionsanker: wenn mittleres element == suchwert
+	if list[len(list)/2] == x {
+
+	}
+
 	return -1
 }
